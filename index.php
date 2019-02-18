ddaf430a19996b4aec1c8b122beea8327698727c
Touhid M Shaikh <touhidshaikh22@gmail.com>
2019-02-18 13:35:17 +0530

    Added Google Analytics
    Added Google Analytics

--- index.php
+++ index.php
@@ -18,6 +18,18 @@
 
     <!-- Custom styles for this template -->
     <link href="css/grayscale.min.css" rel="stylesheet">
+    
+    <!-- GOOGLE Analytics -->
+    <!-- Global site tag (gtag.js) - Google Analytics -->
+    <script async src="https://www.googletagmanager.com/gtag/js?id=UA-131601403-1"></script>
+    <script>
+        window.dataLayer = window.dataLayer || [];
+        function gtag(){dataLayer.push(arguments);}
+        gtag('js', new Date());
+
+        gtag('config', 'UA-131601403-1');
+    </script>
+
 
   </head>
 
